6e9d3017b11d21ce6be83a29fb07fd4ed9d77fb1
fukui12 <2201452@s.asojuku.ac.jp>
2024-01-10 14:37:20 +0900

    i

---
+++ src/db_connect.php
@@ -0,0 +1,7 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1518122-final';
+    const USER = 'LAA1518122';
+    const PASS = 'Pass0312';
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
---
+++ src/toroku.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>購入品登録システム</title>
+</head>
+<body>
+    <form action="toroku_ok.php" method="post">
+        <?php
+        
+        ?>
+    </form>
+</body>
+</html>
