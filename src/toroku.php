26e9dfb2e34878b6e806ce534ff51aefa0e766ec
fukui12 <2201452@s.asojuku.ac.jp>
2024-01-11 10:18:43 +0900

    aa

--- src/toroku.php
+++ src/toroku.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,10 +9,24 @@
     <title>購入品登録システム</title>
 </head>
 <body>
-    <form action="toroku_ok.php" method="post">
-        <?php
-        
-        ?>
-    </form>
+<h2>新規管理者登録</h2>
+    
+<form action="toroku_ok.php" method="post">
+<?php
+    $brand_name=$category_id='';
+    if(isset($_SESSION['Buy'])){
+        $brand_name=$_SESSION['Buy']['brand_name'];
+        $category_id=$_SESSION['Buy']['category_id'];
+    }
+    
+    echo 'ブランド名：<input type="text" name="brand_name" required>';
+    echo 'カテゴリー名：<input type="text" name="category_id" required>';
+?>
+    
+    <p><button type="submit">登録</button></p>
+</form>
+<form action="home.php" method="post">
+    <button type="submit">ホームへ</button>
+</form>
 </body>
 </html>
